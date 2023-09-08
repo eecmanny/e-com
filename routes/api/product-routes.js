351ce28970ca3d9ca619f047b5fc85dff3fe04eb
eecmanny <130661353+eecmanny@users.noreply.github.com>
2023-09-08 14:20:50 -0400

    Need to finish Updating product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+    try {
+    const productData = await { Catagory , Tag }.findAll({
+      include: [
+          {
+              model: Catagory,
+              attributes: ["catagory_name"],
+          },
+          {
+              model: Tag,
+              attributes: ["product_name"],
+          },
+      ],
+  })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+    try {
+    const categoryData = await product.findByPk(req.params.id, {
+      include: [
+          {
+              model: Catagory,
+              attributes: ["catagory_name"],
+          },
+      ],
+  })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
